cb5fb414c615c9563eb7607cf2580e6968f6d197
Amoah Oliver <103007043+amoahoo8@users.noreply.github.com>
2023-07-07 17:33:39 +0000

    Update LoginActivity.java

--- app/src/main/java/com/android/attendance/activity/LoginActivity.java
+++ app/src/main/java/com/android/attendance/activity/LoginActivity.java
@@ -80,7 +80,7 @@ public class LoginActivity extends Activity {
 
 					if (TextUtils.isEmpty(user_name)) 
 					{
-						username.setError("Bad User Name");
+						username.setError("You Typed a Bad User Name");
 					}
 					else if(TextUtils.isEmpty(pass_word))
 					{
